b19d7d2241ce06a019eca61b69ab7942e7f66bdd
Mangesh <mangesh.sontake@gmail.com>
2020-05-04 09:29:38 +0530

    Initial commit

---
+++ src/test/java/FacebookLoginTest.java
@@ -0,0 +1,39 @@
+import java.security.PublicKey;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class FacebookLoginTest {
+
+	 static WebDriver driver;
+	 
+	 @BeforeClass
+	 public void Setup(){
+	 System.setProperty("webdriver.chrome.driver","C:\\SeleniumWebdriver\\chromedriver_win32\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	 }
+	 
+	@Test 
+	public void fblogin(){
+		driver.findElement(By.id("email")).sendKeys("mangesh.sontake@gmail.com");
+	
+		driver.findElement(By.name("pass")).sendKeys("mange");
+		driver.findElement(By.xpath("//input[@value='Log In']")).click();
+	 }
+	 
+	 @AfterClass
+	 @Test
+		public void tearDown() {
+			driver.quit();
+		}
+	 
+}
